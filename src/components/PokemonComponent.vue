0f230c7a58651d9d3bbc89bfb40af580e49d9f5f
Kris Jones <kristop.jones@gmail.com>
2021-05-25 14:23:38 -0600

    prev not working

--- src/components/PokemonComponent.vue
+++ src/components/PokemonComponent.vue
@@ -15,8 +15,8 @@
     </ul>
     <div class="row mt-4">
       <div class="col d-flex justify-content-center">
-        <!-- <button class="mr-4" @click="prev">PREV</button>
-        <button @click="next">NEXT</button> -->
+        <button class="mr-4" @click="prev">PREV</button>
+        <button @click="next">NEXT</button>
       </div>
     </div>
   </div>
@@ -42,7 +42,6 @@ export default {
         pokemonService.getActivePokemon(poke)
       },
       prev() {
-        // pokemonService.prev()
         pokemonService.prev()
       },
       next() {
